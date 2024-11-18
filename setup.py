f8e75772d310df866dc04494b3a5d0b8c8b23ced
anthonyharrison <anthony.p.harrison@gmail.com>
2024-11-18 22:21:23 +0000

    fix: linting

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 
+from bids.version import VERSION
+
 # Copyright (C) 2024 APH10 Limited
 # SPDX-License-Identifier: Apache-2.0
 
-from bids.version import VERSION
 
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
@@ -12,23 +13,23 @@ with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().split("\n")
 
 setup_kwargs = dict(
-    name='bids-analyser',
+    name="bids-analyser",
     version=VERSION,
-    description='Analyser for ELF files',
+    description="Analyser for ELF files",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/aph10/BIDS",
-    author='Anthony Harrison',
-    author_email='anthony@aph10.com',
-    maintainer='Anthony Harrison',
-    maintainer_email='anthony@aph10.com',
-    license='Apache-2.0',
+    author="Anthony Harrison",
+    author_email="anthony@aph10.com",
+    maintainer="Anthony Harrison",
+    maintainer_email="anthony@aph10.com",
+    license="Apache-2.0",
     keywords=["security", "tools", "ELF", "Dependency", "Symbols", "Binary Analsyis"],
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.9",
