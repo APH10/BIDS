7ce1fd7512460b9356790cc461421786f0ec7bf1
anthonyharrison <anthony.p.harrison@gmail.com>
2024-11-18 20:56:14 +0000

    test: correct tests to process file

--- test/test_analyser.py
+++ test/test_analyser.py
@@ -6,7 +6,7 @@ BIDS Analyser tests
 """
 from pathlib import Path
 
-import pytest
+
 
 from bids.analyser import BIDSAnalyser
 
@@ -19,29 +19,34 @@ class TestAnalyser:
     def test_get_global_symbols(self):
         filename = f"{self.TEST_PATH}/test_assets/hello"
         analyser_test = BIDSAnalyser()
+        analyser_test.analyse(filename)
         data = analyser_test.get_global_symbols()
-        assert len(data) == 0
+        assert len(data) > 0
 
     def test_get_local_symbols(self):
         filename = f"{self.TEST_PATH}/test_assets/hello"
         analyser_test = BIDSAnalyser()
+        analyser_test.analyse(filename)
         data = analyser_test.get_local_symbols()
-        assert len(data) == 0
+        assert len(data) > 0
 
     def test_get_dependencies(self):
         filename = f"{self.TEST_PATH}/test_assets/hello"
         analyser_test = BIDSAnalyser()
+        analyser_test.analyse(filename)
         data = analyser_test.get_dependencies()
-        assert len(data) == 0
+        assert len(data) > 0
 
     def test_get_callgraph(self):
         filename = f"{self.TEST_PATH}/test_assets/hello"
         analyser_test = BIDSAnalyser()
+        analyser_test.analyse(filename)
         data = analyser_test.get_callgraph()
         assert len(data) == 0
 
     def test_get_header(self):
         filename = f"{self.TEST_PATH}/test_assets/hello"
         analyser_test = BIDSAnalyser()
+        analyser_test.analyse(filename)
         data = analyser_test.get_header()
-        assert len(data) == 0
+        assert len(data) > 0
